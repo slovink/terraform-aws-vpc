4c1a1ebd26018fa4d496d066a132ca0784b73a00
vinodyadav95 <thevinodyadav95@gmail.com>
2024-01-30 15:19:54 +0530

    update repo

--- _example/versions.tf
+++ _example/versions.tf
@@ -2,7 +2,7 @@
 # Terraform version
 ##----------------------------------------------------------------------------------
 terraform {
-  required_version = ">= 1.6.4, < 1.7.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
