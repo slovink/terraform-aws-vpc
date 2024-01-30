83182c8adecd56112f023f5e5b58311019c4a726
vinodyadav95 <thevinodyadav95@gmail.com>
2024-01-30 14:25:01 +0530

    update repo

---
+++ _example/example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "vpc" {
+  source                = "../."
+  name                  = "yada"
+  environment           = "test"
+  cidr_block            = "10.0.0.0/16"
+  additional_cidr_block = ["192.3.0.0/16", "192.2.0.0/16"]
+}
